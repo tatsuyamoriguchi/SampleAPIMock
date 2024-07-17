6f319c5b6c0e75daa3200f253bea66c0087fa67a
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-17 13:25:32 -0700

    Initial Commit

---
+++ SampleAPIMock/SampleAPIMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAPIMockApp.swift
+//  SampleAPIMock
+//
+//  Created by Tatsuya Moriguchi on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleAPIMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
